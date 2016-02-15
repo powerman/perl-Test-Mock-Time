bc2e876ac77ca192df35609f97a493fc6600d7fe
Alex Efros <powerman@powerman.name>
2016-02-15 20:16:13 +0200

    v0.1.3

--- lib/Test/Mock/Time.pm
+++ lib/Test/Mock/Time.pm
@@ -5,7 +5,7 @@ use strict;
 use utf8;
 use Carp;
 
-our $VERSION = 'v0.1.2';
+our $VERSION = 'v0.1.3';
 
 use Export::Attrs;
 use Scalar::Util qw( weaken );
@@ -494,7 +494,7 @@ Test::Mock::Time - Deterministic time & timers for event loop tests
 
 =head1 VERSION
 
-This document describes Test::Mock::Time version v0.1.2
+This document describes Test::Mock::Time version v0.1.3
 
 
 =head1 SYNOPSIS
